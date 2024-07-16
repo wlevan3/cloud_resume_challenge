e1df59e3492872ea6d6e9bc8ffda4a0abd236b8b
lwj-admin <wjlevan@gmail.com>
2024-07-16 00:11:59 -0700

    block s3 public access

--- infrastructure/modules/frontend/s3.tf
+++ infrastructure/modules/frontend/s3.tf
@@ -38,10 +38,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
