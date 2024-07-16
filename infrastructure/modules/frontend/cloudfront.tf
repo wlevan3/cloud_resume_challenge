843d081c04a4a20bbdfde6624acc17464b94d6e5
lwj-admin <wjlevan@gmail.com>
2024-07-16 02:29:45 -0700

    chg to http

--- infrastructure/modules/frontend/cloudfront.tf
+++ infrastructure/modules/frontend/cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_distribution" "distribution" {
       http_port                = 80
       https_port               = 443
       origin_keepalive_timeout = 5
-      origin_protocol_policy   = "https-only"
+      origin_protocol_policy   = "http-only"
       origin_read_timeout      = 30
       origin_ssl_protocols = [
         "TLSv1.2",
