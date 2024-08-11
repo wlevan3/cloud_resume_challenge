6a8a391fd273a52322b5d9af6efab2402db877e6
lwj-admin <wjlevan@gmail.com>
2024-08-11 01:23:27 -0700

    s3: rm old bucket resources

--- infrastructure/modules/frontend/s3.tf
+++ infrastructure/modules/frontend/s3.tf
@@ -10,11 +10,6 @@ locals {
   current_time  = formatdate("YYYY-MM-DD-hhmmss", timestamp())
 }
 
-# resource "aws_s3_bucket" "website" {
-#   bucket = "${var.s3_bucket_name}-${var.env}"
-# }
-
-
 resource "aws_s3_bucket" "website_domain" {
   bucket = "walterlevan.com"
 }
@@ -52,46 +47,6 @@ resource "aws_s3_bucket" "website_subdomain" {
 }
 
 
-resource "aws_s3_bucket_policy" "allow_access_from_specific_role" {
-  bucket = aws_s3_bucket.website.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.website.arn}/*"
-        ]
-      },
-      {
-        Sid       = "AllowSpecificRoleAccess"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource  = ["${aws_s3_bucket.website.arn}/*", "${aws_s3_bucket.website.arn}"]
-        Condition = {
-          StringEquals = {
-            "aws:PrincipalArn" = "arn:aws:iam::523671527743:role/GithubActionsCICD"
-          }
-        }
-      }
-    ]
-  })
-}
-
-
-# resource "aws_s3_object" "file" {
-#   bucket       = aws_s3_bucket.website.id
-#   key          = "index.html"
-#   content      = data.local_file.index.content
-#   content_type = "text/html"
-# }
 
 resource "aws_s3_bucket_website_configuration" "hosting" {
   bucket = aws_s3_bucket.website_domain.id
@@ -102,6 +57,3 @@ resource "aws_s3_bucket_website_configuration" "hosting" {
 }
 
 
-# data "local_file" "index" {
-#   filename = "${path.root}/../frontend/index.html"
-# }
